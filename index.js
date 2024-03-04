dac2522be5e812290d985929b68e60d6e68069a3
YUSRIN20 <massyusrin123@gmail.com>
2024-03-05 00:54:17 +0530

    second commit

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const PORT = 4000; //http:localhost:4000
 
 // creates file with the name of current timestamp
 app.get("/", (req, res) => {
-  let today = format(new Date(), "dd-mm-yyyy-hh-mm--ss");
+  let today = format(new Date(), "dd-mm-yyyy-hh-mm-ss");
   const filePath = `./TimeStamp/${today}.txt`;
   const fileContent = today;
   fs.writeFileSync(filePath, fileContent, "utf-8");
-  res.status(200).send(`<h1 style="text-align: center;">Current TimeStamp: ${today}</h1><h3 style="text-align: center;">Timestamp has been successfully saved to a folder named (TimeStamp) Change the endpoint to /read to retrieve the all Timestamp data.</h3>` );
+  res.status(200).send(`<h1 style="text-align: center;">Current TimeStamp: ${today}</h1><h3 style="text-align: center;">Timestamp data has been successfully saved to a folder named (TimeStamp) Change the endpoint to /read to retrieve the all Timestamp data.</h3>` );
   
 });
 
