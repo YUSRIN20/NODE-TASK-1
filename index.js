d841f9b20d018930de417ab1ce8f1f6139b0bd09
YUSRIN20 <massyusrin123@gmail.com>
2024-03-04 23:10:47 +0530

    changed h1 tags

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const PORT = 4000;
 let filepath; // filepath for the  endpoint functions
 
 app.get ('/',(req,res)=>{
-    res.status(200).json({message:'code running sucessfully'})
+    res.status(200).send(`<div> <h1 style="text-align: center;">Visit /write Endpoint to save the current Timestamp to a file named TimeStamp.txt </h1> </div>`)
 })
 
 app.get('/write',(req,res)=>{
     let today = format(new Date(),'dd-mm-yyyy-hh-mm-ss')
     filepath = `TimeStamp/${today}.txt`
     fs.writeFileSync(filepath,`${today}`,'utf-8')
-    res.status(200).send(`<div> <h1>Date and time sent successfully to text file change the endpoint to read to get data</h1> </div>`)
+    res.status(200).send(`<div> <h1 style="text-align: center;">Timestamp has been successfully saved to a file named (TimeStamp.txt.) Change the endpoint to /read to retrieve the Timestamp data.</h1> </div>`)
 })
 
 app.get('/read',(req,res)=>{
@@ -23,7 +23,7 @@ app.get('/read',(req,res)=>{
         return res.status(404).send('No file written yet');
     }
     let data = fs.readFileSync(filepath,'utf-8')
-    res.status(200).send(`<h1>${data}</h1>`)
+    res.status(200).send(`<h1 style="text-align: center;">${data}</h1>`)
 })
 
 app.listen(PORT,()=>{
